9c4326ed90dcad93e011a1cf31fa3f1a1adf323b
hitesh <studio@igneus.local>
2017-10-28 14:13:37 +0530

    Login App screen pushed

---
+++ Projects/Login/src/sample/Main.java
@@ -0,0 +1,25 @@
+package sample;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+//        primaryStage.initStyle(StageStyle.UNDECORATED);
+        primaryStage.setTitle("Welcome to LCO");
+        primaryStage.setScene(new Scene(root, 900, 500));
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
